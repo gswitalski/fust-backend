9bf62ee2624edd4d7b34b40e900c6a35e1aea51a
Grzegorz Świtalski <gswitalski@gmail.com>
2023-12-07 16:09:20 +0100

    initial commit

---
+++ src/main/java/pl/grsw/fust/FustApplication.java
@@ -0,0 +1,13 @@
+package pl.grsw.fust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FustApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FustApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/grsw/fust/FustApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grsw.fust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
